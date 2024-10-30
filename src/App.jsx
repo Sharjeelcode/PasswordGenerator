2b923aff2b0567ba4a90982bad3927c219499205
unknown <sharjeelahmed3808@gmail.com>
2024-10-30 18:48:12 +0500

    all project

---
+++ src/App.jsx
@@ -0,0 +1,96 @@
+import { useCallback, useEffect, useRef, useState } from "react";
+
+function App() {
+  const [lenght, setLenght] = useState(8);
+  const [number, setnumber] = useState(false);
+  const [character, setcharacter] = useState(false);
+  const [password, setpassword] = useState("");
+  const [oldPass, setOldPass] = useState([]);
+  const passRef = useRef(null);
+  useEffect(() => {
+    passwordGenerator();
+  }, [lenght, number, character, setpassword]);
+
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if (number) {
+      str += "012345678";
+    }
+    if (character) {
+      str += '!"£$%^&*()_+@';
+    }
+    for (let i = 0; i < lenght; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char);
+    }
+    setpassword(pass);
+  }, [lenght, number, character, setpassword]);
+  const copyPassword = useCallback(() => {
+    window.navigator.clipboard.writeText(password);
+    setOldPass((prev) => [...prev, password]);
+  }, [password, oldPass]);
+  return (
+    <>
+      <h1 className="text-4xl bg-black text-center text-white py-2">
+        Password Generator
+      </h1>
+      <div className="flex justify-center bg-black min-h-[100vh]">
+        <div className="mt-4 ">
+          <div className=" bg-gray-600 rounded-lg p-3 ">
+            <div className="flex ">
+              <input
+                type="text"
+                value={password}
+                className="text-lg w-[100%] outline-none rounded px-2"
+                readOnly
+                ref={passRef}
+              />
+              <button
+                className="p-2 text-white bg-blue-600 rounded"
+                onClick={copyPassword}
+              >
+                Copy
+              </button>
+            </div>
+            <div className="flex text-white gap-3 ">
+              <input
+                type="range"
+                min={8}
+                max={32}
+                value={lenght}
+                onChange={(e) => setLenght(e.target.value)}
+              />
+              <span>Lenght: {lenght}</span>
+              <label htmlFor="Numbers">Numbers</label>
+              <input
+                type="checkbox"
+                id="Numbers"
+                defaultChecked={number}
+                onChange={() => setnumber((prev) => !prev)}
+              />
+              <label htmlFor="Characters">Characters</label>
+              <input
+                type="checkbox"
+                id="Characters"
+                defaultChecked={character}
+                onChange={() => setcharacter((prev) => !prev)}
+              />
+            </div>
+          </div>
+
+          <div className="text-white bg-gray-600 rounded-xl">
+            <h1 className="text-center text-3xl mt-4">Old Passwords</h1>
+            <ul className="p-4 ">
+              {oldPass.map((pass) => (
+                <li>{pass}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
